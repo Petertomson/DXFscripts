48d3d72d8f9e609c9b1aa478663d466b5aa5799f
Petertomson <124280055+Petertomson@users.noreply.github.com>
2023-04-24 16:10:32 +0100

    Add files via upload

--- Adjust_thickness_app.py
+++ Adjust_thickness_app.py
@@ -1,35 +1,25 @@
 import os
 import ezdxf
 import streamlit as st
-import io
-import pandas as pd
-from io import StringIO
-import numpy as np
-from pathlib import Path
-
 
 st.set_page_config(page_title="DXF Viewer", page_icon="📐")
 st.title('DXF adjuster')
 
-#print("This script adjusts the scale of all drawings in a folder to match the thickness of the ply you have")
-
-uploaded_file = st.file_uploader("Choose a .dxf file",type=['dxf'])
+st.write('This app is designed to help adjust drawings so friction joints are not too tight or too loose. To do this the app scales the drawing.')
+uploaded_file = st.file_uploader("To start choose a .dxf file", type=['dxf'])
 
 if uploaded_file is not None:
     fullname = uploaded_file.name
     folder_path = '.'
     uploaded_file_path = os.path.join(folder_path, uploaded_file.name)
-    st.write(uploaded_file_path)
     with open(os.path.join(folder_path, uploaded_file.name), "wb") as f:
         f.write(uploaded_file.getbuffer())
 if uploaded_file is not None:
-    ply_thickness_str = st.text_input('Ply thickness', '18')
+    #After uploading ask the user for the values for their sheet material and drawing. The decimal input option
+    ply_thickness = st.number_input('Enter the thickness of the sheet material you are cutting: ', 0.0, 200.0, 18.0, 0.01)
+    drawing_thickness = st.number_input('Enter the width of the slots in your drawing as a decimal: ', 0.0, 200.0, 18.0, 0.01)
 
-    drawing_thickness_str = st.text_input('Enter the width of the slots in your drawing as a decimal: ', '18')
-
-    ply_thickness = float(ply_thickness_str)
-    drawing_thickness = float(drawing_thickness_str)
-    
+    #Finish choices
     finish_option = st.selectbox(
         'What finish are you going to use?',
         ('Varnish', 'Stain', 'Oil', 'Osmo', 'None'))
@@ -46,33 +36,31 @@ if uploaded_file is not None:
 
     st.write('Your finish will add: ', finish_adjust, 'mm')
 
+    #Sort out adjustment values#
     adjust_val = (ply_thickness - finish_adjust) / drawing_thickness
     adjust_number = drawing_thickness - ply_thickness + finish_adjust
-    st.write('Your joints in your drawing will be adjusted by: ', adjust_number, 'mm')
-    # Prompt the user for a folder path
-    #uploaded_file = input("Please select a .dxf file: ")
-    # Check dxf then adjust
-    # Construct the full file path
-    #file_path = os.path.join(uploaded_file)
-    #file_base = file_path.removesuffix('.dxf')
 
-    # Open the file with ezdxf
+    if adjust_number < 0:
+        st.write('Your joints in your drawing will be increased by: ', adjust_number*-1, 'mm')
+    elif adjust_number == 0:
+        st.write('Your joints in your drawing will not be changed')
+    else:
+        st.write('Your joints in your drawing will be reduced by: ', adjust_number, 'mm')
+
+    # button to start converting process
     if st.button('Adjust .DXF'):
         if uploaded_file is not None:
-            #st.write(uploaded_file)
+            #Read the file using ezdxf
             doc = ezdxf.readfile(uploaded_file_path)
 
             # Select all elements and scale
             st.write('Your drawing will be scaled by ', adjust_val)
             for entity in doc.modelspace():
                 entity.scale(adjust_val, adjust_val, 1)
-
-            # Set the units to mm
-            # doc.header['$INSUNITS'] = ezdxf.lldxf.UNITS['mm']
+            #Sort out the file name
             basename = uploaded_file_path.removesuffix('.dxf')
             uploaded_file_mod = basename + " " + str(adjust_number) + "mm.dxf"
             doc.saveas(uploaded_file_mod)
+            #Load the binary data from the file as f then add it to a download button
             with open(uploaded_file_mod, 'rb') as f:
-                st.download_button('Download adjusted file',f ,  uploaded_file_mod)
-
-#print("Batch processing complete.")
+                st.download_button('Download adjusted file', f,  uploaded_file_mod)
